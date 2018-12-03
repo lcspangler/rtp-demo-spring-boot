8b7ec01320371041d6132340180a1609cd4b1ed9
Liz Spangler <lizspan@gmail.com>
2018-12-03 03:57:51 -0500

    producer in rtp mock route

--- rtp-demo-mock/src/main/java/rtp/demo/mock/routes/MockRtpRouteBuilder.java
+++ rtp-demo-mock/src/main/java/rtp/demo/mock/routes/MockRtpRouteBuilder.java
@@ -1,14 +1,62 @@
 package rtp.demo.mock.routes;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.kafka.KafkaComponent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MockRtpRouteBuilder extends RouteBuilder {
 
+	private static final Logger LOG = LoggerFactory.getLogger(MockRtpRouteBuilder.class);
+
+	// @Value("${BOOTSTRAP_SERVERS}")
+	private String kafkaBootstrap = System.getenv("BOOTSTRAP_SERVERS");
+
+	// @Value("${PRODUCER_TOPIC}")
+	private String kafkaProducerTopic = System.getenv("PRODUCER_TOPIC");
+
+	// CamelContext camelContext = new DefaultCamelContext();
+
 	@Override
 	public void configure() throws Exception {
-		from("timer://foo?period=5000").setBody().constant("Hello World!").log(">>> ${body}");
+		LOG.info("Configuring Mock RTP Routes");
+		// PropertiesComponent pc = getContext().getComponent("properties",
+		// PropertiesComponent.class);
+		// pc.setLocation("classpath:application.properties");
+
+		KafkaComponent kafka = new KafkaComponent();
+
+		kafka.setBrokers("172.30.123.84:9092");
+
+		this.getContext().addComponent("kafka", kafka);
+
+		LOG.info("COMPONENTS: " + this.getContext().getComponentNames());
+
+		// camelContext.addComponent("kafka", kafka);
+
+		from("timer://foo?period=5000").setBody().constant("Hello World! " + kafkaBootstrap)
+				.log(">>> ${body}" + "/////" + kafkaBootstrap + "  " + kafkaProducerTopic + "COMPONENTS: "
+						+ this.getContext().getComponentNames() + "kafka component: "
+						+ this.getContext().getComponent("kafka"))
+				.to("kafka:my-topic-1");
+	}
+
+	public String getKafkaBootstrap() {
+		return kafkaBootstrap;
+	}
+
+	public void setKafkaBootstrap(String kafkaBootstrap) {
+		this.kafkaBootstrap = kafkaBootstrap;
+	}
+
+	public String getKafkaProducerTopic() {
+		return kafkaProducerTopic;
+	}
+
+	public void setKafkaProducerTopic(String kafkaProducerTopic) {
+		this.kafkaProducerTopic = kafkaProducerTopic;
 	}
 
 }
