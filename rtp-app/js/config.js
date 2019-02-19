c18d487d1f469d7726cfda9d8067410d2a9d338f
anurag saran <anuragsaran@anurags-MacBook-Pro-2.local>
2019-02-19 11:58:57 -0500

    added second app and updated API's

--- rtp-app/js/config.js
+++
@@ -1,7 +0,0 @@
-
-var appConfig = {
-    "API": {
-        "transaction": "http://rtp-debtor-payment-service-fsi-real-time-payments.6923.rh-us-east-1.openshiftapps.com/transactions-service/transactions",
-        "payment": "http://rtp-debtor-payment-service-fsi-real-time-payments.6923.rh-us-east-1.openshiftapps.com/payments-service/payments"
-    }
-}
